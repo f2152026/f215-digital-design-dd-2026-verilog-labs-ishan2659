98d6ece9af9bb3e0ff1874008f9836a5ca07361d
ishan2659 <ishanjakhar2005@gmail.com>
2026-09-19 15:27:22 +0000

    lab02 task1

---
+++ labs/lab02/task1/dut.v
@@ -0,0 +1,29 @@
+// dut.v
+// Top-level wrapper so the same tb.v can test either implementation.
+// Exactly ONE of the two instantiations below should be uncommented at a
+// time. Comment out the other one, save, and re-run the simulation.
+
+module DUT (
+  input  I0,
+  input  I1,
+  input  S,
+  output Y
+);
+
+  // Dataflow version
+  mux_df U1 (
+    .I0 (I0),
+    .I1 (I1),
+    .S  (S),
+    .Y  (Y)
+  );
+
+  // Behavioral version
+  // mux_beh U1 (
+  //   .I0 (I0),
+  //   .I1 (I1),
+  //   .S  (S),
+  //   .Y  (Y)
+  // );
+
+endmodule
